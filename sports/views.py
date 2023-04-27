73681cd7a47e0017ad250fea2c8336df770a944b
mibachcha1 <68137662+mibachcha1@users.noreply.github.com>
2023-04-27 18:45:52 -0500

    Created test page with working scores

--- sports/views.py
+++ sports/views.py
@@ -1,3 +1,21 @@
+from nba_api.live.nba.endpoints import scoreboard
+from easydict import EasyDict as edict
 from django.shortcuts import render
+import requests
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def test(request):
+    games = scoreboard.ScoreBoard()
+    all_games = edict(games.get_dict())
+    game_Ids = []
+    toReturn = {}
+    for game in all_games.scoreboard.games:
+        game_Ids.append(game.gameId)
+        toReturn[game.gameId] = {"home" : game.homeTeam, "away" : game.awayTeam}
+
+    context = {'ids' : game_Ids, 'games' : toReturn}
+    return render(request, 'test.html', context)
