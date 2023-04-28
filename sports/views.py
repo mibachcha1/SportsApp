4a765b80923441f472cbc3be900d05676a754070
mibachcha1 <68137662+mibachcha1@users.noreply.github.com>
2023-04-28 02:03:24 -0500

    Play by Play update

--- sports/views.py
+++ sports/views.py
@@ -2,14 +2,42 @@ from nba_api.live.nba.endpoints import scoreboard
 from easydict import EasyDict as edict
 from django.shortcuts import render
 import requests
+import urllib3
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html')    
 
 def play_by_play(request):
-    return render(request, 'playbyplay.html')
+    game_id = request.GET.get('id')
+    games = scoreboard.ScoreBoard()
+    all_games = edict(games.get_dict())
+    current_game = {}
+    for game in all_games.scoreboard.games:
+        if game.gameId == game_id:
+            current_game = game
+    
+    home_team_name = current_game.homeTeam.teamName
+    away_team_name = current_game.awayTeam.teamName
+    home_team_id =  current_game.homeTeam.teamId
+    away_team_id =  current_game.awayTeam.teamId
+    
+    home_wins = str(current_game.homeTeam.wins) + "-" + str(current_game.homeTeam.losses)
+    away_wins = str(current_game.awayTeam.wins) + "-" + str(current_game.awayTeam.losses)
+    series = current_game.seriesText
+    context = {
+        "homeTeamName": home_team_name,
+        "awayTeamName": away_team_name,
+        "homeTeamId" : home_team_id,
+        "awayTeamId" : away_team_id,
+        "gameId" : game_id,
+        "homeW" : home_wins,
+        "awayW" : away_wins,
+        "series" : series,
+    }
+    
+    return render(request, 'playbyplay.html', context)
 
 def test(request):
     games = scoreboard.ScoreBoard()
